98c6c199223af324111c6c63263d8897e25cf260
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2021-06-14 17:56:38 +0530

    Added navbar, landing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
